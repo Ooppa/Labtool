7bb43e229ae0c2e5856727c04d4e68df5f29752a
Ooppa <ooppa@ooppa.fi>
2016-02-14 23:35:59 +0200

    @ComponentScan to main-class

--- labtool/src/main/java/fi/javalabra/labtool/application/LabtoolApplication.java
+++ labtool/src/main/java/fi/javalabra/labtool/application/LabtoolApplication.java
@@ -2,15 +2,18 @@ package fi.javalabra.labtool.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  *
  * @author Ooppa
  */
 @SpringBootApplication
+@ComponentScan("fi.javalabra.labtool")
 public class LabtoolApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LabtoolApplication.class, args);
     }
+    
 }
